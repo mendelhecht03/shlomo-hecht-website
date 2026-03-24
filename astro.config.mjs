ee23c61912e981e5598c05680384e53f69956956
Mendel Hecht <mendelhecht016@gmail.com>
2026-03-24 01:01:35 -0400

    Revert "Swap to Players Club musician template"
    This reverts commit c1f9851e19ab1f480f0cb85de548f790fb8587cc.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import sitemap from '@astrojs/sitemap';
-
 // https://astro.build/config
-export default defineConfig({
-  devToolbar: {
-    enabled: false,
-  },
-  prefetch: true,
-
-  site: 'https://shlomo-hecht-website.netlify.app/',
-
-  integrations: [sitemap()],
-});
+export default defineConfig({});
